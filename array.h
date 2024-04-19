e4ed6165992ee76b74c95d45ddc9500e63b4695c
Damian Swaczyna <damianswa12@gmail.com>
2024-04-19 14:09:28 +0200

    commit 1

---
+++ array.h
@@ -0,0 +1,20 @@
+#ifndef ARRAY2_ARRAY_H
+#define ARRAY2_ARRAY_H
+
+#define SIZE 10
+
+void displayArray(int array[]);
+
+int findMaximumValue(int array[]);
+
+int saveArrayToFile(int array[]);
+
+int findMinimumValue(int array[]);
+
+int enterDataIntoArray(int array[]);
+
+float calculateAverege(int array[]);
+
+void displayFromFile(int array[]);
+
+#endif //ARRAY2_ARRAY_H
